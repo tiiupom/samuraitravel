489fe013ff656d3b1196558e16ebb51086db19e9
tiiupom <chococo.lav1013.pom@gmail.com>
2024-11-28 10:47:41 +0900

    初期samuraitravel

---
+++ samuraitravel/src/main/java/com/example/samuraitravel/security/UserDetailsImpl.java
@@ -0,0 +1,68 @@
+package com.example.samuraitravel.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.samuraitravel.entity.User;
+
+// implementsキーワードを使いUserDetailsインターフェースを実装
+		// 実装することで、インターフェースに定義されている抽象メソッドを上書きし、具体的な処理内容を作成する必要がある
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+    private final Collection<GrantedAuthority> authorities;
+
+    public UserDetailsImpl(User user, Collection<GrantedAuthority> authorities) {
+        this.user = user;
+        this.authorities = authorities;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    // ハッシュ化済みのパスワードを返す
+    @Override	// UserDetailsインターフェースに定義されている抽象メソッドを上書き
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    // ログイン時に利用するユーザー名（メールアドレス）を返す
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    // ロールのコレクションを返す
+    @Override
+    		// 戻り値<? ectends A>は、「Aまたはそのサブタイプ(ある型から派生した型)全て」の意味
+    		// GrantedAuthority　＝　ユーザーに割り当てられたロール(権限)を表すインターフェース
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    // アカウントが期限切れでなければtrueを返す
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // ユーザーがロックされていなければtrueを返す
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // ユーザーのパスワードが期限切れでなければtrueを返す
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // ユーザーが有効であればtrueを返す
+    @Override
+    public boolean isEnabled() {
+        return user.getEnabled();
+    }
+}
